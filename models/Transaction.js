c7a57b410c1fea5119005d698866e15243b82dcf
Tejas Gorde <tejasgorde67@gmail.com>
2025-07-28 13:20:21 +0530

    Fix: Define schema in Transaction model

--- models/Transaction.js
+++ models/Transaction.js
@@ -1,15 +1,35 @@
 const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
-  playerId: String,
-  usdAmount: Number,
-  cryptoAmount: Number,
-  currency: String,
-  transactionType: { type: String, enum: ["bet", "cashout"] },
-  transactionHash: String,
-  priceAtTime: Number,
-  timestamp: { type: Date, default: Date.now },
+  playerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Player",
+    required: true,
+  },
+  roundId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Round",
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  multiplier: {
+    type: Number,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["bet", "cashout"],
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports =
-  mongoose.models.ModelName || mongoose.model("ModelName", schema);
+  mongoose.models.Transaction ||
+  mongoose.model("Transaction", transactionSchema);
